805ae21306a1ac4d47d81ea460a5c8ae6693b821
controlvisas-collab <controlvisas@gmail.com>
2026-03-24 15:23:22 -0700

    page.js

--- src/app/display/page.js
+++ src/app/display/page.js
@@ -1,11 +1,9 @@
 "use client";
 
 export default function Display() {
-  console.log("DISPLAY FUNCIONANDO");
-
   return (
-    <div className="h-screen flex items-center justify-center text-4xl">
-      DISPLAY OK
+    <div style={{fontSize: "40px", padding: "40px"}}>
+      DISPLAY OK 🔥
     </div>
   );
 }
